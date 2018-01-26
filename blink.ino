62eb811b042de4ca720ecd4bd25a0095fb5b5e79
Japbore <japbore@gmail.com>
2018-01-26 16:25:19 +0100

    Añadidos TODO

--- blink.ino
+++ blink.ino
@@ -26,6 +26,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  //TODO: Configure read pin
 }
 
 // the loop function runs over and over again forever
@@ -34,4 +35,5 @@ void loop() {
   delay(2000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);                       // wait for a second
+  //TODO: read pin and do something
 }
